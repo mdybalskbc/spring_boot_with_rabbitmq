f8f0f2ee619d9d44ce88ccf52120f17753da7f69
mdybalskbc <mdybalsk@visa.com>
2025-05-19 16:02:44 +0200

    First commit

---
+++ src/test/java/pl/coderslab/springbootwithrabbitmq/SpringBootWithRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.coderslab.springbootwithrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithRabbitMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
